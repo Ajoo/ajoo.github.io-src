0ebdb6b75a3da3467459bf1806abe9d6731ba80c
Ajoo <ajoo@outlook.pt>
2016-12-18 01:00:10 +0000

    eddited links

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,14 +5,12 @@ from __future__ import unicode_literals
 AUTHOR = 'Ajoo'
 SITENAME = 'Wintermute Dev Logs'
 SITEURL = ''
-
-PATH = 'content'
+GITHUB_URL = 'https://github.com/Ajoo/ajoo.github.io-src'
 
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = 'English'
 
-GITHUB_URL = 'https://github.com/Ajoo/ajoo.github.io-src'
+PATH = 'content'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,9 +20,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
+LINKS = (('kaggle', 'https://www.kaggle.com'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
